c3f449a1c3bb8baf6634156afabeeb6b65763ddc
Nitin Kumar <nkumar@tillster.com>
2023-03-08 21:14:47 +0530

    Nitin's Portfolio

---
+++ README.md
@@ -0,0 +1,1 @@
+# https://nitinnitin.github.io/portfolio/
---
+++ main.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+
+// jQuery for list Section
+// handles the hide and show of list options
+  $('#expTab').addClass('list-card-active');
+
+  $('#education').hide();
+  $('#skills').hide();
+
+
+  $('#expTab').click(function() {
+
+    $('#expTab').addClass('list-card-active');
+    $('#eduTab').removeClass('list-card-active');
+    $('#skillsTab').removeClass('list-card-active');
+
+    $('#experience').show();
+    $('#education').hide();
+    $('#skills').hide();
+  });
+
+  $('#eduTab').click(function() {
+
+    $('#eduTab').addClass('list-card-active');
+    $('#expTab').removeClass('list-card-active');
+    $('#skillsTab').removeClass('list-card-active');
+
+    $('#education').show();
+    $('#experience').hide();
+    $('#skills').hide();
+  });
+
+  $('#skillsTab').click(function() {
+
+    $('#skillsTab').addClass('list-card-active');
+    $('#eduTab').removeClass('list-card-active');
+    $('#expTab').removeClass('list-card-active');
+
+    $('#skills').show();
+    $('#education').hide();
+    $('#experience').hide();
+  });
+
+  $('#intTab').click(function() {
+
+    $('#eduTab').removeClass('list-card-active');
+    $('#expTab').removeClass('list-card-active');
+    $('#skillsTab').removeClass('list-card-active');
+
+    $('#education').hide();
+    $('#experience').hide();
+    $('#skills').hide();
+  });
+
+});
+
+
+// Dark Mode Implemented using DOM Manipulation of css variables
+
+const colorWhite = '#ffffff';
+const bgColorDark = '#282c35';
+const bgColorLight = '#edecec';
+const itColor = '#959595';
+const primeColor = '#5a5a5a';
+const secondColor = '#7c746c';
+const shadowDarkOnLight = '#cccccc';
+const shadowLightOnLight = '#fefefe';
+const shadowDarkOnDark = '#22252d';
+const shadowLightOnDark = '#2e333d';
+
+let root = document.documentElement;
+
+const mode = document.querySelector('.mode');
+const darkMode = document.querySelector('#dark-mode');
+const lightMode = document.querySelector('#light-mode');
+
+function changeModeToDark () {
+  darkMode.classList.add('hide');
+  lightMode.classList.remove('hide');
+
+  darkMode.classList.remove('show');
+  lightMode.classList.add('show');
+  root.style.setProperty('--background-color', bgColorDark);
+  root.style.setProperty('--it-color', colorWhite);
+  root.style.setProperty('--prime-color', colorWhite);
+  root.style.setProperty('--second-color', colorWhite);
+  root.style.setProperty('--shadow-dark', shadowDarkOnDark);
+  root.style.setProperty('--shadow-light', shadowLightOnDark);
+
+}
+
+function changeModeToLight () {
+  lightMode.classList.add('hide');
+  darkMode.classList.add('show');
+
+  lightMode.classList.remove('show');
+  darkMode.classList.remove('hide');
+  root.style.setProperty('--background-color', bgColorLight);
+  root.style.setProperty('--it-color', itColor);
+  root.style.setProperty('--prime-color', primeColor);
+  root.style.setProperty('--second-color', secondColor);
+  root.style.setProperty('--shadow-dark', shadowDarkOnLight);
+  root.style.setProperty('--shadow-light', shadowLightOnLight);
+
+}
+
+darkMode.addEventListener('click', changeModeToDark);
+lightMode.addEventListener('click', changeModeToLight);
